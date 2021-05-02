f008cde5c162fbb93abdd79141a4b91af8c0ea70
Jason Rizo <jasonrizo84@gmail.com>
2021-05-02 18:27:42 -0500

    WAR configuration

--- src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPruebaApplication.java
+++ src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPruebaApplication.java
@@ -2,10 +2,18 @@ package co.edu.unicundi.proyectoSpringPrueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProyectoSpringPruebaApplication {
+public class ProyectoSpringPruebaApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(ProyectoSpringPruebaApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringPruebaApplication.class, args);
 	}
