0c57480f9682dc34cbb5b8db0d699625aab20d6a
Jason Rizo <jasonrizo84@gmail.com>
2021-05-02 20:09:29 -0500

    Controller added

---
+++ src/main/java/co/edu/unicundi/proyectoSpringPrueba/controller/JenkinsController.java
@@ -0,0 +1,49 @@
+package co.edu.unicundi.proyectoSpringPrueba.controller;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+import co.edu.unicundi.proyectoSpringPrueba.dto.ExceptionResponse;
+import co.edu.unicundi.proyectoSpringPrueba.entity.Consulta;
+import co.edu.unicundi.proyectoSpringPrueba.exception.FieldValidationException;
+import co.edu.unicundi.proyectoSpringPrueba.exception.ObjectNotFoundException;
+import co.edu.unicundi.proyectoSpringPrueba.exception.RepeatedObjectException;
+import co.edu.unicundi.proyectoSpringPrueba.service.imp.IConsultaService;
+import co.edu.unicundi.proyectoSpringPrueba.service.imp.IProfesorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
+@RestController
+@RequestMapping("/prueba")
+@Api(value = "Servicios Rest de de prueba para servidor tomcat")
+public class JenkinsController {
+
+	
+	@GetMapping("/jenkins1")
+	public ResponseEntity<?> pruebaUno() {
+		return new ResponseEntity<Object>("Hola Tomcat", HttpStatus.OK);
+	}
+
+	
+}
